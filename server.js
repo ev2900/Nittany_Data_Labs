4a482f4f54bf3a8f78ce8c5e2d6e8ef970c67eaa
Christopher Sharkey <ev2900@gmail.com>
2017-03-17 09:35:01 -0400

    Update the redirect

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 	
 	//EJS Routes
 	app.get('/', function (req, res) {
-	  res.redirect('https://www.facebook.com/events/1775548189439442/');
+	  res.redirect('https://www.facebook.com/events/387638481617450/');
 	});
 
 	//EJS Routes
